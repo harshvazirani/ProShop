5dd02c3222628a02d9076d27a4df897d4b48989e
Harsh Vazirani <harshvazirani7@gmail.com>
2021-10-23 12:57:13 +0530

    Authentication added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose  from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema({
     name: {
@@ -19,6 +20,10 @@ const userSchema = mongoose.Schema({
         default: false
     },
 }, {timestamps: true})
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+       return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
